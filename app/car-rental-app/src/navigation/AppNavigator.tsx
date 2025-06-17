d014df2f97d65b7cbffbc64916439f5ace1b3f29
bujar-rakipi <bujarrakipi05@gmail.com>
2025-06-17 13:29:16 +0200

    fixed error

--- app/car-rental-app/src/navigation/AppNavigator.tsx
+++ app/car-rental-app/src/navigation/AppNavigator.tsx
@@ -1,4 +1,3 @@
-import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import React from 'react';
 import CarDetails from '../pages/CarDetails';
@@ -25,14 +24,12 @@ const Stack = createStackNavigator<RootStackParamList>();
 
 const AppNavigator = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="CarDetails" component={CarDetails} />
-        <Stack.Screen name="MyRentals" component={MyRentals} />
-        <Stack.Screen name="Search" component={Search} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Stack.Navigator initialRouteName="Home">
+      <Stack.Screen name="Home" component={Home} />
+      <Stack.Screen name="CarDetails" component={CarDetails} />
+      <Stack.Screen name="MyRentals" component={MyRentals} />
+      <Stack.Screen name="Search" component={Search} />
+    </Stack.Navigator>
   );
 };
 
